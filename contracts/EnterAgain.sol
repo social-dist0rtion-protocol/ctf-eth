5c557e4d22ab07e04511d5d97f820c69162a3eeb
vrde <agranzot@gmail.com>
2018-12-29 23:23:15 +0100

    fix filenames

--- contracts/EnterAgain.sol
+++ contracts/EnterAgain.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-import "./entrance.sol";
+import "./Entrance.sol";
 import "./SafeMath.sol";
 
 contract EnterAgain {
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 // @format
-var fs = require('fs');
+var fs = require("fs");
 
-var Entrance = artifacts.require('./entrance.sol');
-var EnterAgain = artifacts.require('./enteragain.sol');
+var Entrance = artifacts.require("./Entrance.sol");
+var EnterAgain = artifacts.require("./EnterAgain.sol");
 
 module.exports = function(deployer, network) {
   deployer.deploy(Entrance, 0xbc4f77).then(instance => {
